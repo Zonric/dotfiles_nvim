88494ee4ed1df39deab75eb483be7fd92614e09d
Zonric <zonric42@gmail.com>
2025-01-09 20:14:13 -0500

    Reduce the width of neo-tree window Autoclose if last window.

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -8,5 +8,14 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	}
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			window = {
+				position = "left",
+				width = 30,
+			},
+		})
+	end,
 }
