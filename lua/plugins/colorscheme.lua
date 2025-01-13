e7f404905c836437b3b286089bca62a0c523a0a2
Zonric <zonric42@gmail.com>
2025-01-12 21:16:24 -0500

    Change cursorline highlighting

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,8 @@ return {
 		priority = 999,
 		config = function()
 			vim.cmd.colorscheme "nightfox"
+			vim.api.nvim_set_hl(0, "CursorLine", { bg = "#131a24", underline = true, })
 		end,
 	},
 }
+
