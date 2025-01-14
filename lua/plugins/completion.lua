4cb4c5c81c632ded467e4ef5202ec6b8643e0e9c
zonric <zonric42@gmail.com>
2025-01-13 20:30:10 -0500

    Add LazyDev, Remove react-snipeets

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -7,6 +7,16 @@ return {
 		dependencies = {
 			{
 				{
+					"folke/lazydev.nvim",
+					ft = "lua",
+					opts = {
+						library = {
+							{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+							{ path = "lazy.nvim", words = { "LazyVim" } },
+						},
+					},
+				},
+				{
 					"L3MON4D3/LuaSnip",
 					build = "make install_jsregexp",
 					dependencies = {
@@ -17,27 +27,71 @@ return {
 						history = true,
 					},
 				},
+			},{
+				"onsails/lspkind.nvim",
+				opts = {
+					mode = "symbol_text",
+					preset = "codicons",
+					symbol_map = {
+						Text = "󰉿",
+						Method = "󰆧",
+						Function = "󰊕",
+						Constructor = "",
+						Field = "󰜢",
+						Variable = "󰀫",
+						Class = "󰠱",
+						Interface = "",
+						Module = "",
+						Property = "󰜢",
+						Unit = "󰑭",
+						Value = "󰎠",
+						Enum = "",
+						Keyword = "󰌋",
+						Snippet = "",
+						Color = "󰏘",
+						File = "󰈙",
+						Reference = "󰈇",
+						Folder = "󰉋",
+						EnumMember = "",
+						Constant = "󰏿",
+						Struct = "󰙅",
+						Event = "",
+						Operator = "󰆕",
+						TypeParameter = "",
+						Codeium = "󰆨",
+					},
+				},
 			},
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
-			"mlaursen/vim-react-snippets",
-
-			-- For ultisnips users.
-			--"SirVer/ultisnips"
-			--"quangnguyen30192/cmp-nvim-ultisnips"
 		},
-
 		config = function()
-			--require("vim-react-snippets").lazy_load()
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
 			luasnip.config.setup {
 				require("luasnip.loaders.from_vscode").lazy_load()
 			}
 
 			cmp.setup {
+---@diagnostic disable-next-line: missing-fields
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = 'symbol_text',
+						maxwidth = {
+							menu = 50,
+							abbr = 50,
+						},
+						ellipsis_char = '...',
+						show_labelDetails = true,
+---@diagnostic disable-next-line: unused-local
+						before = function (entry, vim_item)
+							return vim_item
+						end
+					})
+				},
 				snippet = {
 					expand = function(args)
 						luasnip.lsp_expand(args.body)
@@ -46,6 +100,7 @@ return {
 				completion = { completeopt = "menu,menuone,noinsert" },
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
+					{ name = "lazydev", group_index = 0, },
 					{ name = "codeium" },
 					{ name = "luasnip" },
 					{ name = "path" },
