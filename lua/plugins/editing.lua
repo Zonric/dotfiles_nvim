dcbba1d312aaea6f082f94063571a9edeecfc117
Zonric <zonric42@gmail.com>
2026-09-22 10:07:18 -0400

    feat(editing): add nvim-ts-context-commentstring for jsx comments

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -37,5 +37,21 @@ return {
 		},
 		opts = {},
 	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		enabled = true,
+		lazy = true,
+		opts = {
+			enable_autocmd = false,
+		},
+		init = function()
+			local get_option = vim.filetype.get_option
+			vim.filetype.get_option = function(filetype, option)
+				return option == "commentstring"
+					and require("ts_context_commentstring.internal").calculate_commentstring()
+					or get_option(filetype, option)
+			end
+		end,
+	},
 }
 
