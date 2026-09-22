5f9443829f3081ff1c819775f27ed9a8e04ac679
Zonric <zonric42@gmail.com>
2026-09-22 09:41:41 -0400

    refactor(config): streamline startup order and deduplicate options

--- init.lua
+++ init.lua
@@ -1,11 +1,5 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
-
-require("config.lazy")
-
 require("config.options")
+require("config.lazy")
 require("config.keymaps")
 require("config.autocmd")
-
 require("config.diagnostics")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,7 @@
 --- Global Setttings:
 local g = vim.g
+g.mapleader = " "
+g.maplocalleader = "\\"
 g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
 g.php_indent_disable = 1
