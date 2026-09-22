95f40c7d4647336ea154989c7528ebb1971d3e09
Zonric <zonric42@gmail.com>
2026-09-22 13:11:20 -0400

    feat(themes): highlight cursorword in bright bold pink

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -7,7 +7,9 @@ return {
 		opts = {},
 		config = function()
 			vim.cmd("colorscheme carbonfox")
-			vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#FFFFFF"})
+			vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#FFFFFF" })
+			vim.api.nvim_set_hl(0, "MiniCursorword", { fg = "#ff7eb6", bold = true, underline = true })
+			vim.api.nvim_set_hl(0, "MiniCursorwordCurrent", { fg = "#ff7eb6", bold = true, underline = true })
 		end,
 	},
 }
