aa1a5166e0dbe6056b4acd93e39b46853e878c77
Zonric <zonric42@gmail.com>
2026-09-22 09:51:44 -0400

    feat(lsp): configure ts_ls inlay hints and add <leader>ch toggle keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -36,6 +36,34 @@ return {
 					},
 				},
 			})
+
+			local lspconfig = require("lspconfig")
+			lspconfig.ts_ls.setup({
+				settings = {
+					typescript = {
+						inlayHints = {
+							includeInlayParameterNameHints = "all",
+							includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+							includeInlayFunctionParameterTypeHints = true,
+							includeInlayVariableTypeHints = true,
+							includeInlayPropertyDeclarationTypeHints = true,
+							includeInlayFunctionLikeReturnTypeHints = true,
+							includeInlayEnumMemberValueHints = true,
+						},
+					},
+					javascript = {
+						inlayHints = {
+							includeInlayParameterNameHints = "all",
+							includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+							includeInlayFunctionParameterTypeHints = true,
+							includeInlayVariableTypeHints = true,
+							includeInlayPropertyDeclarationTypeHints = true,
+							includeInlayFunctionLikeReturnTypeHints = true,
+							includeInlayEnumMemberValueHints = true,
+						},
+					},
+				},
+			})
 		end,
 	},
 	{
